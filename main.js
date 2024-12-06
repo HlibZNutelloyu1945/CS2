a2fca07c08a4ac184b6d059daf44e39ac11c7d6e
vseponovoy <vseponovoy23@gmail.com>
2024-12-06 19:01:37 +0200

    hello

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let logo = document.querySelector(".logo");
+  let clickCount = 0;
+  const sound = new Audio("the-rake-scream.mp3");
+  let gif = document.querySelector("#gif");
+
+  logo.addEventListener("click", function () {
+    clickCount += 1;
+    if (clickCount == 50) {
+      sound.play();
+      clickCount = 0;
+      gif.classList.remove("is-hidden");
+      setTimeout(function () {
+        gif.classList.add("is-hidden");
+      }, 2000);
+    }
+  });
+});
